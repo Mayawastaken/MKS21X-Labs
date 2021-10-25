b3d7759de33fac3cc705eeb8c4ce656988126499
Maya Doron-Repa <mdoron-repa30@stuy.edu>
2021-10-25 11:42:07 -0400

    added point

---
+++ 02-Triangle/Point.java
@@ -0,0 +1,38 @@
+public class Point{
+  private double x,y;
+  public Point(double X, double Y){
+    x=X;
+    y=Y;
+  }
+  public Point(Point p){
+    x= p.x;
+    y= p.y;
+  }
+  public double getX(){
+    return x;
+  }
+  public double getY(){
+    return y;
+  }
+
+  public static double distance(Point one, Point two){
+    double x1 = one.getX();
+    double y1 = one.getY();
+    double x2 = two.getX();
+    double y2 = two.getY();
+    return Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+  }
+
+  public double distanceTo(Point two){
+    double x1 = x;
+    double y1 = y;
+    double x2 = two.getX();
+    double y2 = two.getY();
+    return Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+  }
+
+  public String toString(){
+    return "(" + x + ", " + y + ")";
+  }
+
+}
