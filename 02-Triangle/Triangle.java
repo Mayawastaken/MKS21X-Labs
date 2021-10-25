fdc12442fc7db1d32f6fa7ff477546aeb871c913
Maya Doron-Repa <mdoron-repa30@stuy.edu>
2021-10-25 19:33:02 -0400

    added constructors

--- 02-Triangle/Triangle.java
+++ 02-Triangle/Triangle.java
@@ -1,3 +1,17 @@
 public class Triangle {
-  
+  private Point v1, v2, v3;
+
+  public Triangle(Point x, Point y, Point z){
+    v1 = x;
+    v2 = y;
+    v3 = z;
+  }
+  public Triangle(Triangle t){
+    v1 = t.v1;
+    v2 = t.v2;
+    v3 = t.v3;
+  }
+  //public Point getVertex(int i){ er
+
+  //} hoiiii 
 }
