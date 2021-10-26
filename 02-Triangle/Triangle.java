1dfbbca45b426962d3fecb361f84c63214ac893e
Maya Doron-Repa <mdoron-repa30@stuy.edu>
2021-10-25 20:57:23 -0400

    added get n set

--- 02-Triangle/Triangle.java
+++ 02-Triangle/Triangle.java
@@ -6,12 +6,35 @@ public class Triangle {
     v2 = y;
     v3 = z;
   }
+
   public Triangle(Triangle t){
     v1 = t.v1;
     v2 = t.v2;
     v3 = t.v3;
   }
-  //public Point getVertex(int i){ er
 
-  //} hoiiii 
+  public Point getVertex(int i){
+    if (i == 1){
+      return v1;
+    }
+    if (i == 2){
+      return v2;
+    }
+    if (i == 3){
+      return v3;
+    }
+    return v1;
+  }
+
+  public void setVertex(int i, Point p){
+    if (i == 1){
+      v1 = p;
+    }
+    if (i == 2){
+      v2 = p;
+    }
+    if (i == 3){
+      v3 = p;
+    }
+  }
 }
