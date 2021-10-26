668f442ff58fda4b8e8a2d45d58a9aa494b79875
Maya Doron-Repa <mdoron-repa30@stuy.edu>
2021-10-25 21:02:58 -0400

    added triangle w doubles pt2

--- 02-Triangle/Triangle.java
+++ 02-Triangle/Triangle.java
@@ -11,8 +11,10 @@ public class Triangle {
     Point x = new Point(x1, y1);
     Point y = new Point(x2, y2);
     Point z = new Point(x3, y3);
-    Triangle(x, y, z);
-    
+    v1 = x;
+    v2 = y;
+    v3 = z;
+
   }
   public Triangle(Triangle t){
     v1 = t.v1;
