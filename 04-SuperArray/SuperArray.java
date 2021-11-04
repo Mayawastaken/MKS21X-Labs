8982ead062e472104e94917a6ce10c686789399f
Maya Doron-Repa <mdoron-repa30@stuy.edu>
2021-11-04 19:41:49 -0400

    added a tester n size n superarray constructor

--- 04-SuperArray/SuperArray.java
+++ 04-SuperArray/SuperArray.java
@@ -1,4 +1,15 @@
 public class SuperArray{
+  private String[] data;
+  private int size;
 
-  
+  public SuperArray(){
+    size = 0;
+    data = new String[10];
+  }
+
+  public int size(){
+    return size;
+  }
+
+
 }
---
+++ 04-SuperArray/SuperTester.java
@@ -0,0 +1,9 @@
+public class SuperTester{
+  public static void main(String[]args){
+    SuperArray SA = new SuperArray();
+
+
+    System.out.println(SA);
+
+  }
+}
