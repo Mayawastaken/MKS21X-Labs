e8a5cad038ba90f305a088c31191f7dc0284c306
Maya Doron-Repa <mdoron-repa30@stuy.edu>
2021-11-04 20:04:37 -0400

    added add w cases

--- 04-SuperArray/SuperArray.java
+++ 04-SuperArray/SuperArray.java
@@ -7,9 +7,35 @@ public class SuperArray{
     data = new String[10];
   }
 
+  public String toString(){
+    String str = "[";
+    for (int i = 0; i < size; i++){
+      str += '"' + data[i] + '"';
+      if (i < size - 1){
+        str += ", ";
+      }
+    }
+    str += "]";
+    return str;
+  }
+
+
+  public String[] getData(){
+    return data;
+  }
+
   public int size(){
     return size;
   }
 
+  public void add(String str){
+    for (int i = 0; i < (this.getData()).length; i++){
+      if (this.getData()[i] == null){
+        this.getData()[i] = str;
+        size++;
+        break;
+      }
+    }
+  }
 
 }
--- 04-SuperArray/SuperTester.java
+++ 04-SuperArray/SuperTester.java
@@ -3,7 +3,13 @@ public class SuperTester{
     SuperArray SA = new SuperArray();
 
 
-    System.out.println(SA);
+    System.out.println(SA.toString());
+    SA.add("hello");
+    System.out.println(SA.toString());
+    SA.add("wow");
+    SA.add("it");
+    SA.add("works!");
+    System.out.println(SA.toString());
 
   }
 }
