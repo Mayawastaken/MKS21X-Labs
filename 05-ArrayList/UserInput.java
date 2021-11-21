c5ea9b868012f501a5b0f4d95572e2a5732d6519
Maya Doron-Repa <mdoron-repa30@stuy.edu>
2021-11-21 16:11:19 -0500

    added mean min max + case for size() = 0

--- 05-ArrayList/UserInput.java
+++ 05-ArrayList/UserInput.java
@@ -13,13 +13,35 @@ public class UserInput{
       }
       else{
         System.out.println("You entered: " + nonNegDoubles.toString());
-        System.out.println("Mean: ");
-        System.out.println("Min value: ");
-        System.out.println("Max value ");
+        if (nonNegDoubles.size() > 0){
+          double min = nonNegDoubles.get(0);
+          double max = nonNegDoubles.get(0);
+          for (int i = 1; i < nonNegDoubles.size(); i++){
+            if (nonNegDoubles.get(i) < min){
+              min = nonNegDoubles.get(i);
+            }
+            if (nonNegDoubles.get(i) > max){
+              max = nonNegDoubles.get(i);
+            }
+          }
+          double sum = 0;
+          for (int i = 0; i < nonNegDoubles.size(); i++){
+            sum += nonNegDoubles.get(i);
+          }
+          double mean = sum / nonNegDoubles.size();
+          System.out.println("Mean: " + mean);
+          System.out.println("Min value: " + min);
+          System.out.println("Max value " + max);
+        }
+        else {
+          System.out.println("Mean: N/A");
+          System.out.println("Min value: N/A");
+          System.out.println("Max value: N/A");
+        }
+
         input.close();
         break;
       }
     }
-
   }
 }
