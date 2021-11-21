b5752f5d153c75b7f85a430712c9e7b289434b9b
Maya Doron-Repa <mdoron-repa30@stuy.edu>
2021-11-21 16:06:00 -0500

    added input code, just need min max mean

--- 05-ArrayList/UserInput.java
+++ 05-ArrayList/UserInput.java
@@ -2,6 +2,24 @@ import java.util.Scanner;
 import java.util.ArrayList;
 public class UserInput{
   public static void main(String args[]){
+    System.out.println("Enter a non negative number. A negative number will end the program: ");
     Scanner input = new Scanner(System.in);
+    ArrayList<Double> nonNegDoubles = new ArrayList<Double>();
+    while(input.hasNextDouble()){
+      System.out.println("Enter a non negative number. A negative number will end the program: ");
+      double checker = input.nextDouble();
+      if (checker >= 0){
+        nonNegDoubles.add(checker);
+      }
+      else{
+        System.out.println("You entered: " + nonNegDoubles.toString());
+        System.out.println("Mean: ");
+        System.out.println("Min value: ");
+        System.out.println("Max value ");
+        input.close();
+        break;
+      }
+    }
+
   }
 }
