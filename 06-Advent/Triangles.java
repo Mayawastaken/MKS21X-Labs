87739e97a55025a57975e249bd23ebe01f5c6f1d
Maya Doron-Repa <mdoron-repa30@stuy.edu>
2021-11-22 11:44:38 -0500

    made p1 own function

--- 06-Advent/Triangles.java
+++ 06-Advent/Triangles.java
@@ -3,11 +3,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class Triangles{
-  public static void main(String[] args){
+  public static int part1(String filename){
+    int counterP1 = 0;
     try{
-      File file = new File("Triangles.txt");
+      File file = new File(filename);
       Scanner input = new Scanner(file);
-      int counterP1 = 0;
       while (input.hasNextInt()){
         int s1 = input.nextInt();
         int s2 = input.nextInt();
@@ -16,11 +16,14 @@ public class Triangles{
           counterP1++;
         }
       }
-      System.out.println("Part I number of triangles: " + counterP1);
       input.close();//releases the file from your program
     } catch (FileNotFoundException ex) {
             //File not found
-        }
+    }
+    return counterP1;
+  }
+  public static void main(String[] args){
+    System.out.println("Part I number of triangles: " + part1("Triangles.txt"));
     }
 }
 
