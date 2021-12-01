41c4dd5575c5a6202d00bdec45767e42672b06ca
Maya Doron-Repa <mdoron-repa30@stuy.edu>
2021-11-30 21:03:19 -0500

    added pt 2 yay

--- 06-Advent/Non.java
+++ 06-Advent/Non.java
@@ -53,15 +53,56 @@ public class Non{
         if (vowels3(current) && doubles(current) && withoutStuff(current)){
           needed++;
         }
-//add close, catch etc return
-
       }
-
-
-
+      input.close();
+      return needed;
+    } catch (FileNotFoundException ex) {
+      //File not found
+      return 0;
     }
+  }
 
+  public static boolean pairs(String str){
+    String checker = "";
+    for (int i = 0; i < str.length() - 1; i++){
+      checker = str.substring(i, i+2);
+      for (int j = i+2; j < str.length() - 1; j++){
+        if (checker.equals(str.substring(j, j+2))){
+          return true;
+        }
+      }
+    }
+    return false;
+  }
 
+  public static boolean sandwich(String str){
+    char checker = 'a';
+    for (int i = 0; i < str.length() - 2; i++){
+      checker = str.charAt(i);
+      if (checker == str.charAt(i+2)){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public static int part2(String filename){
+    try{
+      int needed = 0;
+      File file = new File(filename);
+      Scanner input = new Scanner(file);
+      while (input.hasNext()){
+        String current = input.next();
+        if (pairs(current) && sandwich(current)){
+          needed++;
+        }
+      }
+      input.close();
+      return needed;
+    } catch (FileNotFoundException ex) {
+      //File not found
+      return 0;
+    }
   }
 
   public static void main(String[] args){
@@ -77,23 +118,6 @@ public class Non{
     } catch (ArrayIndexOutOfBoundsException ex){
       System.out.println("no acceptable file given and/or part given :(");
     }
-
-
   }
-
-//   public static boolean doublePT2(String str){
-//     String checker = "";
-//     for (int i = 0; i < str.length() - 1; i++){
-//       checker = str.substring(i, i+2);
-//       for (int j = i+2; i < str.length() - 1; i++){
-//         if
-//       }
-//
-//     }
-//   }
-//
-// }
-
-
 
 }
