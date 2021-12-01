26fd2a9ea46d6495b8543acdd559e1e932f296c6
Maya Doron-Repa <mdoron-repa30@stuy.edu>
2021-11-30 20:47:46 -0500

    fixed it

--- 07-BinarySearch/BinarySearch.java
+++ 07-BinarySearch/BinarySearch.java
@@ -1,9 +1,10 @@
+import java.util.Arrays;
 public class BinarySearch{
   public static int binarySearch(int[] data, int target){
     int start = 0;
-    int end = data.length;
+    int end = data.length - 1;
     int mid = 43110;
-    while (start < end){
+    while (start <= end){
       mid = (start + end) / 2;
       if (data[mid] == target){
         return mid;
@@ -15,12 +16,12 @@ public class BinarySearch{
         end = mid - 1;
       }
     }
-    if ((start == end) && (start == 0) && (data[start] == data[end])){
-     return start;
-    }
-    if ((start == end) && (start == data.length-1) && (data[start] == data[end])){
-     return start;
-    }
+    // if ((start == end) && (start == 0) && (data[start] == target)){
+    //  return start;
+    // }
+    // if ((start == end) && (start == data.length-1) && (data[start] == target)){
+    //  return start;
+    // }
     return -1;
   }
 
@@ -36,6 +37,25 @@ public class BinarySearch{
     //...
     result = binarySearch(new int[]{0,1,2,3,4,5,6},6);
     System.out.println(result);//should be 6
+
+    // Custom input array, but you can make it randomly
+    int[] arr = { 13, 7, 6, 45, 21, 9, 101, 102 };
+
+    System.out.println(Arrays.toString(arr));
+    //Arrays.sort() changes the array, no return value
+    Arrays.sort(arr);
+    System.out.println(Arrays.toString(arr));
+
+    System.out.println(" ");
+    System.out.println(binarySearch(arr, 6));
+    System.out.println(binarySearch(arr, 7));
+    System.out.println(binarySearch(arr, 9));
+    System.out.println(binarySearch(arr, 13));
+    System.out.println(binarySearch(arr, 21));
+    System.out.println(binarySearch(arr, 45));
+    System.out.println(binarySearch(arr, 101));
+    System.out.println(binarySearch(arr, 102));
+    System.out.println(binarySearch(arr, -102));
   }
 
 
