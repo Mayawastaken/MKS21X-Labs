23a1fc303049aec95de0460bfd447b28c7f3c470
Maya Doron-Repa <mdoron-repa30@stuy.edu>
2021-12-03 11:38:55 -0500

    wrote selec sort, seems to work will test more at home

--- 08-Sorts/Sorts.java
+++ 08-Sorts/Sorts.java
@@ -22,6 +22,35 @@ public class Sorts{
   } //yo whyd i make it so complex below
 
 
+  /*Selection sort
+   */
+  public static void selectionSort(int[] ary){
+    int maya = 0;
+    int checker = ary[0];
+    int index = -1; //like if we r at the lowest alr the if statemnet wont run so we gotta set index = maya or smth
+    //System.out.println(checker);
+    while (maya < ary.length){
+      index = -1;
+      checker = ary[maya];
+      //System.out.println("while: " + checker);
+      for (int i = maya + 1; i < ary.length; i++){
+        if (ary[i] < checker){
+          checker = ary[i];
+          index = i;
+          //System.out.println(checker);
+        }
+      }
+      if (index != -1){
+        ary[index] = ary[maya];
+        ary[maya] = checker;
+      }
+      //System.out.println(checker);
+      maya++;
+    }
+  }
+
+
+
 
   // public static void bubbleSortAH(int[] data){
   //   int checker = 43110;
@@ -47,23 +76,44 @@ public class Sorts{
   //   }
   // }
 
-  public static void main(String[] args){
-    int[] ary1 = {0,1,2,3,4,-1};
-    int[] ary2 = {4,2,6,8,-12,34,2,3,-4};
-    int[] ary3 = {0,1,0,2,0,0};
-    int[] ary4 = {0,1,2,3,4};
-    int[] ary5 = {5,1,12,-5,16};
+  public static void main(String[] args){ //make method later w random for easier tests; many lengths & tests
+    int[] ary1B = {0,1,2,3,4,-1};
+    int[] ary2B = {4,2,6,8,-12,34,2,3,-4};
+    int[] ary3B = {0,1,0,2,0,0};
+    int[] ary4B = {0,1,2,3,4};
+    int[] ary5B = {5,1,12,-5,16};
 
-    bubbleSort(ary1);
-    bubbleSort(ary2);
-    bubbleSort(ary3);
-    bubbleSort(ary4);
-    bubbleSort(ary5);
+    bubbleSort(ary1B);
+    bubbleSort(ary2B);
+    bubbleSort(ary3B);
+    bubbleSort(ary4B);
+    bubbleSort(ary5B);
 
-    System.out.println(Arrays.toString(ary1));
-    System.out.println(Arrays.toString(ary2));
-    System.out.println(Arrays.toString(ary3));
-    System.out.println(Arrays.toString(ary4));
-    System.out.println(Arrays.toString(ary5));
+    System.out.println(Arrays.toString(ary1B));
+    System.out.println(Arrays.toString(ary2B));
+    System.out.println(Arrays.toString(ary3B));
+    System.out.println(Arrays.toString(ary4B));
+    System.out.println(Arrays.toString(ary5B));
+
+    System.out.println(" ");
+
+    int[] ary1S = {0,1,2,3,4,-1};
+    int[] ary2S = {4,2,6,8,-12,34,2,3,-4};
+    int[] ary3S = {0,1,0,2,0,0};
+    int[] ary4S = {0,1,2,3,4};
+    int[] ary5S = {5,1,12,-5,16};
+
+    selectionSort(ary1S);
+    selectionSort(ary2S);
+    selectionSort(ary3S);
+    selectionSort(ary4S);
+    selectionSort(ary5S);
+
+    System.out.println(Arrays.toString(ary1S));
+    System.out.println(Arrays.toString(ary2S));
+    System.out.println(Arrays.toString(ary3S));
+    System.out.println(Arrays.toString(ary4S));
+    System.out.println(Arrays.toString(ary5S));
+
   }
 }
