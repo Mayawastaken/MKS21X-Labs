4403dba798f2af7b4ba890d80d24ac2403058f84
Maya Doron-Repa <mdoron-repa30@stuy.edu>
2021-12-06 20:26:30 -0500

    thinking ab it after food helped a lot - insert done

--- 08-Sorts/Driver.java
+++ 08-Sorts/Driver.java
@@ -16,11 +16,17 @@ import java.util.Arrays;
         //if it works this is for timing purposes.
         Sorts.bubbleSort(randish);
       }
+      if(artie[1].equals("insertion")){
+        //if it works this is for timing purposes.
+        Sorts.insertionSort(randish);
+      }
       if(artie[1].equals("test")){
         //if you need to check if it works this is for verification of correctness
+        int[] randish1 = Arrays.copyOf(randish,randish.length);
         int[] randish2 = Arrays.copyOf(randish,randish.length);
         int[] randish3 = Arrays.copyOf(randish,randish.length);
         Sorts.selectionSort(randish);
+        Sorts.insertionSort(randish1);
         Sorts.bubbleSort(randish2);
         Arrays.sort(randish3);
         if( Arrays.equals(randish,randish3)){
@@ -32,6 +38,11 @@ import java.util.Arrays;
           System.out.println("Bubble Correct!");
         }else{
          System.out.println("Bubble BROKEN!!!!");
+        }
+        if(Arrays.equals(randish1,randish3)){
+          System.out.println("Insertion Correct!");
+        }else{
+         System.out.println("Insertion BROKEN!!!!");
         }
       }
     }
--- 08-Sorts/Sorts.java
+++ 08-Sorts/Sorts.java
@@ -49,52 +49,17 @@ public class Sorts{
     }
   }
 
-  public static void insertionSort(int[]data){ //wat even going on 0_0 LMAO will fix at home prob rewrite cant think <33
-    int maya = 1;
-    while (maya < data.length){
-      int j = maya;
-      for (int i = maya; i < data.length; i++){
-        for (int p = j-1; p > -1; p--){
-          if (data[maya] < data[p]){
-            j--;
-          }
-        }
-      System.out.println(j);
+  public static void insertionSort(int[]data){
+    for (int i = 1; i < data.length; i++){
+      int checker = data[i];
+      int j = i-1;
+      while (j >= 0 && data[j] > checker){
+        data[j+1] = data[j];
+        j--;
       }
-      int storage = data[maya];
-      for (int m = j; m <= maya; m++){
-        data[m+1] = data[m];
-
-      }
-      data[j] = data[maya];
-      maya++;
+      data[j+1] = checker;
     }
   }
-
-
-  // public static void bubbleSortAH(int[] data){
-  //   int checker = 43110;
-  //   int wanted = 1;
-  //   for (int i = 0; i < data.length - wanted; i++){
-  //     checker = data[0];
-  //     //System.out.println(checker);
-  //     for (int j = i + 1; j < data.length - wanted + 1; j++){
-  //       if (checker > data[j]){
-  //         data[j-1] = data[j];
-  //         data[j] = checker;
-  //         //System.out.println(checker);
-  //         System.out.println(Arrays.toString(data));
-  //       }
-  //       else {
-  //         checker = data[j];
-  //         System.out.println(checker);
-  //         //wanted++; //wot is wanted why did i make this
-  //         //j += data.length;
-  //       }
-  //     }
-  //     wanted++;
-  //   }
-  // }
 
   public static void main(String[] args){ //make method later w random for easier tests; many lengths & tests
     int[] ary1B = {0,1,2,3,4,-1};
