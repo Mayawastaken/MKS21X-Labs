fe560fd845010fc4afa41b55603ec21d5d589843
Maya Doron-Repa <mdoron-repa30@stuy.edu>
2021-12-17 11:43:47 -0500

    started ordered, up to add methods for that

--- 11-ExtendArrayList/NoNullArrayList.java
+++ 11-ExtendArrayList/NoNullArrayList.java
@@ -1,9 +1,38 @@
 import java.util.ArrayList;
 public class NoNullArrayList<T> extends ArrayList<T> {
+
+  public NoNullArrayList(){
+    super();
+  }
+
+  public NoNullArrayList(int  startingCapacity){
+    super(startingCapacity);
+  }
+
+  public void add(int index, T element) throws IllegalArgumentException{
+    if (!(element == null)){
+      super.add(index, element);
+    }
+    else{
+      throw new IllegalArgumentException("null given in set");
+    }
+  }
+
+
+  public boolean add(T element) throws IllegalArgumentException{
+    if (!(element == null)){
+      return super.add(element);
+    }
+    else{
+      throw new IllegalArgumentException("null given in set");
+    }
+  }
+
+
   //wuhoh well um imma try to fix at  home n add constructors <3
-  public T set(int index, T str){
+  public T set(int index, T str) throws IllegalArgumentException{
     if (!(str == null)){
-      super.set(index, str);
+      return super.set(index, str);
     }
     else{
       throw new IllegalArgumentException("null given in set");
