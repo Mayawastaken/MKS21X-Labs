b9b46a6cf74f9a5794607533d137e9871a7c52cb
mayawastaken <mayadoronrepa@gmail.com>
2022-01-11 11:37:57 -0500

    made a border (task 1)

--- 12-Stuyablo/Border.java
+++ 12-Stuyablo/Border.java
@@ -3,9 +3,22 @@ public class Border{
     Text.hideCursor();
     Text.clear();
     Text.go(1,1);
-    for (int i = 1; i < 81; i++){
+    for (int i = 1; i < 80; i++){
       System.out.print(Text.colorize("*",Text.MAGENTA));
     }
+    for (int i = 1; i < 30; i++){ //um check if its 30x80 and not smth else0
+      System.out.print(Text.colorize("*",Text.MAGENTA));
+      Text.go(i,80);
+    }
+    for (int i = 80; i > 0; i--){ //um check if its 30x80 and not smth else0
+      System.out.print(Text.colorize("*",Text.MAGENTA));
+      Text.go(30,i);
+    }
+    for (int i = 30; i > 0; i--){ //um check if its 30x80 and not smth else0
+      System.out.print(Text.colorize("*",Text.MAGENTA));
+      Text.go(i,1);
+    }
+    Text.go(31,1); //get terminal back down
   }
 
 }
